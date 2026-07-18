a5d4f9d9ce450875910d378eaf33f53afd4dd9fb
tjohncan <171094307+tjohncan@users.noreply.github.com>
2026-07-18 18:10:52 -0400

    dbt: apply standard override for system generate_schema_name macro

---
+++ dbt/star_schema/macros/generate_schema_name.sql
@@ -0,0 +1,8 @@
+-- Use custom schemas as-is (no target-schema prefix)
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- if custom_schema_name is none -%}
+        {{ target.schema }}
+    {%- else -%}
+        {{ custom_schema_name | trim }}
+    {%- endif -%}
+{%- endmacro %}
